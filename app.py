ae042fb1e025751df3be592e64238d2792e29453
yash-raj202134 <yashraj3376@gmail.com>
2024-03-18 14:30:03 +0530

    gemini response configuration

---
+++ app.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+load_dotenv() ## load all the environemnt variables
+
+import streamlit as st
+import os
+import sqlite3
+
+import google.generativeai as genai
+
+## Configure Genai Key
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function To Load Google Gemini Model and provide queries as response
+
+def get_gemini_response(question,prompt):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content([prompt[0],question])
+    return response.text
+
+
+## Function To retrieve query from the database
+def read_sql_query(sql,db):
+    connection = sqlite3.connect(db)
+
+    cur = connection.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    connection.commit()
+    connection.close()
+
+    for row in rows:
+        print(row)
+
+    return rows
+
+
+# Defining the prompt ..\
+
+prompt =[
+    """
+    You are an expert in converting English questions to SQL query!
+    The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
+    SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
+    the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
+    \nExample 2 - Tell me all the students studying in Data Science class?, 
+    the SQL command will be something like this SELECT * FROM STUDENT 
+    where CLASS="Data Science"; 
+    also the sql code should not have ``` in beginning or end and sql word in output
+
+    """
+
+]
+
+## Streamlit application
