9731cf09265490c4e1ec40326bec0e3fbc73a718
yash-raj202134 <yashraj3376@gmail.com>
2024-03-18 14:18:20 +0530

    database setup

---
+++ sql.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+# connection to sqlite
+
+connection = sqlite3.connect("student.db")
+
+
+
+# Create a cursor object to insert record , create table, retrieve 
+
+cursor = connection.cursor()
+
+# Create a table
+
+table_info = """
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25),MARKS INT);
+
+"""
+
+cursor.execute(table_info)
+
+# Insert some records
+
+cursor.execute('''Insert Into STUDENT values('Yash','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('Shubham','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Rishika','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('Pratyush','DEVOPS','A',50)''')
+cursor.execute('''Insert Into STUDENT values('Ankit','DEVOPS','A',35)''')
+
+# Display all the records
+
+print("The inserted records are : ")
+
+data = cursor.execute("""Select * From STUDENT""")
+
+for row in data:
+    print(row)
+
+
+# Close the connection
+connection.commit()
+connection.close()
